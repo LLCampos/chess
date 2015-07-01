527386ea546f6e4fdf2d539b352ca482a2f74cf7
LLCampos <luis1212@sapo.pt>
2015-07-01 09:10:40 +0100

    added possible_moves_king method

--- lib/chess.rb
+++ lib/chess.rb
@@ -1,3 +1,49 @@
+class Piece
+
+  def initialize(symbol, color, type, position)
+    @symbol = symbol
+    @color = color
+    @position = position
+    @possible_moves = possible_moves(type, position)
+  end
+
+  def possible_moves(type, position)
+    case type
+    when king
+      possible_moves_king(position)
+    when queen
+      possible_moves_queen(position)
+    when rook
+      possible_moves_rook(position)
+    when bishop
+      possible_moves_bishop(position)
+    when knight
+      possible_moves_knight(position)
+    when pawn
+      possible_moves_pawn(position)
+    end
+  end
+
+  def possible_moves_king(position)
+    pm = []
+    [-1, 0, 1].each do |line|
+      [-1, 0, 1].each do |column|
+        pm << [position[0] + line, position[1] + column]
+      end
+    end
+    legal_moves(position, pm)
+  end
+end
+
+
+class ChessGame
+  def initialize
+  end
+end
+
+
+
+
 w_king = "\u2654"
 w_queen = "\u2655"
 w_rook = "\u2656"
