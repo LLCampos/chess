b3cfa3b613adfa5ed1adb92710d3607c54d3333e
LLCampos <luis1212@sapo.pt>
2015-07-01 15:16:07 +0100

    added show method and correted other little things

--- lib/chess.rb
+++ lib/chess.rb
@@ -1,70 +1,63 @@
 require_relative "pieces"
 
 class ChessGame
+  attr_accessor :board
+
   def initialize
     @board = [
-                [Piece.new(@b_rook, 'black', 'rook', [0, 0]),
-                 Piece.new(@b_knight, 'black', 'knight', [0, 1]),
-                 Piece.new(@b_bishop, 'black', 'bishop', [0, 2]),
-                 Piece.new(@b_king, 'black', 'king', [0, 3]),
-                 Piece.new(@b_queen, 'black', 'queen', [0, 4]),
-                 Piece.new(@b_bishop, 'black', 'bishop', [0, 5]),
-                 Piece.new(@b_knight, 'black', 'knight', [0, 6]),
-                 Piece.new(@b_rook, 'black', 'rook', [0, 7])],
+                [Piece.new("\u265C", 'black', 'rook', [0, 0]),
+                 Piece.new("\u265E", 'black', 'knight', [0, 1]),
+                 Piece.new("\u265D", 'black', 'bishop', [0, 2]),
+                 Piece.new("\u265A", 'black', 'king', [0, 3]),
+                 Piece.new("\u265B", 'black', 'queen', [0, 4]),
+                 Piece.new("\u265D", 'black', 'bishop', [0, 5]),
+                 Piece.new("\u265E", 'black', 'knight', [0, 6]),
+                 Piece.new("\u265C", 'black', 'rook', [0, 7])],
 
-                [Piece.new(@b_pawn, 'black', 'pawn', [1, 0]),
-                 Piece.new(@b_pawn, 'black', 'pawn', [1, 1]),
-                 Piece.new(@b_pawn, 'black', 'pawn', [1, 2]),
-                 Piece.new(@b_pawn, 'black', 'pawn', [1, 3]),
-                 Piece.new(@b_pawn, 'black', 'pawn', [1, 4]),
-                 Piece.new(@b_pawn, 'black', 'pawn', [1, 5]),
-                 Piece.new(@b_pawn, 'black', 'pawn', [1, 6]),
-                 Piece.new(@b_pawn, 'black', 'pawn', [1, 7])],
+                [Piece.new("\u265F", 'black', 'pawn', [1, 0]),
+                 Piece.new("\u265F", 'black', 'pawn', [1, 1]),
+                 Piece.new("\u265F", 'black', 'pawn', [1, 2]),
+                 Piece.new("\u265F", 'black', 'pawn', [1, 3]),
+                 Piece.new("\u265F", 'black', 'pawn', [1, 4]),
+                 Piece.new("\u265F", 'black', 'pawn', [1, 5]),
+                 Piece.new("\u265F", 'black', 'pawn', [1, 6]),
+                 Piece.new("\u265F", 'black', 'pawn', [1, 7])],
 
                 [' '] * 8,
                 [' '] * 8,
                 [' '] * 8,
                 [' '] * 8,
 
-                [Piece.new(@w_pawn, 'white', 'pawn', [6, 0]),
-                 Piece.new(@w_pawn, 'white', 'pawn', [6, 1]),
-                 Piece.new(@w_pawn, 'white', 'pawn', [6, 2]),
-                 Piece.new(@w_pawn, 'white', 'pawn', [6, 3]),
-                 Piece.new(@w_pawn, 'white', 'pawn', [6, 4]),
-                 Piece.new(@w_pawn, 'white', 'pawn', [6, 5]),
-                 Piece.new(@w_pawn, 'white', 'pawn', [6, 6]),
-                 Piece.new(@w_pawn, 'white', 'pawn', [6, 7])],
+                [Piece.new("\u2659", 'white', 'pawn', [6, 0]),
+                 Piece.new("\u2659", 'white', 'pawn', [6, 1]),
+                 Piece.new("\u2659", 'white', 'pawn', [6, 2]),
+                 Piece.new("\u2659", 'white', 'pawn', [6, 3]),
+                 Piece.new("\u2659", 'white', 'pawn', [6, 4]),
+                 Piece.new("\u2659", 'white', 'pawn', [6, 5]),
+                 Piece.new("\u2659", 'white', 'pawn', [6, 6]),
+                 Piece.new("\u2659", 'white', 'pawn', [6, 7])],
 
-                [Piece.new(@w_rook, 'white', 'rook', [7, 0]),
-                 Piece.new(@w_knight, 'white', 'knight', [7, 1]),
-                 Piece.new(@w_bishop, 'white', 'bishop', [7, 2]),
-                 Piece.new(@w_king, 'white', 'king', [7, 3]),
-                 Piece.new(@w_queen, 'white', 'queen', [7, 4]),
-                 Piece.new(@w_bishop, 'white', 'bishop', [7, 5]),
-                 Piece.new(@w_knight, 'white', 'knight', [7, 6]),
-                 Piece.new(@w_rook, 'white', 'rook', [7, 7])]
+                [Piece.new("\u2656", 'white', 'rook', [7, 0]),
+                 Piece.new("\u2658", 'white', 'knight', [7, 1]),
+                 Piece.new("\u2657", 'white', 'bishop', [7, 2]),
+                 Piece.new("\u2654", 'white', 'king', [7, 3]),
+                 Piece.new("\u2655", 'white', 'queen', [7, 4]),
+                 Piece.new("\u2657", 'white', 'bishop', [7, 5]),
+                 Piece.new("\u2658", 'white', 'knight', [7, 6]),
+                 Piece.new("\u2656", 'white', 'rook', [7, 7])]
              ]
   end
 
   def show
-    @board.each do |line|
-      puts '| ' + line.join(' | ') + ' |'
+    puts '   ' + (1..8).to_a.join('  ')
+    board.each_with_index do |line, nline|
+      line_symbol = line.map { |position| position == ' ' ? ' ' : position.symbol }
+      puts "#{nline + 1} |" + line_symbol.join(' |') + ' |'
     end
   end
+
 end
 
 
 
 
-@w_king = "\u2654"
-@w_queen = "\u2655"
-@w_rook = "\u2656"
-@w_bishop = "\u2657"
-@w_knight = "\u2658"
-@w_pawn = "\u2659"
-@b_king = "\u265A"
-@b_queen = "\u265B"
-@b_rook = "\u265C"
-@b_bishop = "\u265D"
-@b_knight = "\u265E"
-@b_pawn = "\u2659F"
--- lib/pieces.rb
+++ lib/pieces.rb
@@ -1,32 +1,47 @@
 class Piece
+  attr_accessor :symbol, :position, :possible_moves
+
   def initialize(symbol, color, type, position)
     @symbol = symbol
     @color = color
     @position = position
-    @possible_moves = possible_moves(type, position)
+    @possible_moves = possible_next_moves(type, position)
   end
+
+  @w_king = "\u2654"
+  @w_queen = "\u2655"
+  @w_rook = "\u2656"
+  @w_bishop = "\u2657"
+  @w_knight = "\u2658"
+  @w_pawn = "\u2659"
+  @b_king = "\u265A"
+  @b_queen = "\u265B"
+  @b_rook = "\u265C"
+  @b_bishop = "\u265D"
+  @b_knight = "\u265E"
+  @b_pawn = "\u2659F"
 
 
 end
 
-def possible_moves(type, position)
+def possible_next_moves(type, position)
   case type
   when 'king'
-    possible_moves_king(position)
+    possible_next_moves_king(position)
   when 'queen'
-    possible_moves_queen(position)
+    possible_next_moves_queen(position)
   when 'rook'
-    possible_moves_rook(position)
+    possible_next_moves_rook(position)
   when 'bishop'
-    possible_moves_bishop(position)
+    possible_next_moves_bishop(position)
   when 'knight'
-    possible_moves_knight(position)
+    possible_next_moves_knight(position)
   when 'pawn'
-    possible_moves_pawn(position)
+    possible_next_moves_pawn(position)
   end
 end
 
-def possible_moves_king(position)
+def possible_next_moves_king(position)
   pm = []
   [-1, 0, 1].each do |line|
     [-1, 0, 1].each do |column|
@@ -36,10 +51,10 @@ def possible_moves_king(position)
   legal_moves(position, pm)
 end
 
-def possible_moves_pawn(position)
+def possible_next_moves_pawn(position)
 end
 
-def possible_moves_bishop(position)
+def possible_next_moves_bishop(position)
   pm = []
   (-7..7).each do |n|
     pm << [position[0] + n, position[1] + n]
@@ -49,7 +64,7 @@ def possible_moves_bishop(position)
   legal_moves(position, pm)
 end
 
-def possible_moves_rook(position)
+def possible_next_moves_rook(position)
   pm = []
   (1..7).each do |n|
     pm << [position[0], position[1] + n]
@@ -60,11 +75,11 @@ def possible_moves_rook(position)
   legal_moves(position, pm)
 end
 
-def possible_moves_queen(position)
-  possible_moves_rook(position) + possible_moves_bishop(position)
+def possible_next_moves_queen(position)
+  possible_next_moves_rook(position) + possible_next_moves_bishop(position)
 end
 
-def possible_moves_knight(position)
+def possible_next_moves_knight(position)
   pm = []
   [-2, -1, 1, 2].each do |x|
     [-2, -1, 1, 2].each do |y|
@@ -80,3 +95,5 @@ def legal_moves(position, pm)
     move == position || move[0] > 7 || move[1] > 7 || move[0] < 0 || move[1] < 0
   end
 end
+
+
