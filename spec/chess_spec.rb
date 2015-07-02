4c60be46825c118b97a119c697823b199cab9f73
LLCampos <luis1212@sapo.pt>
2015-07-02 15:45:31 +0100

    added tests for "move_legality methods"

--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -1,24 +1,53 @@
 require 'chess.rb'
 
-describe '#occupied_spaces'  do
+describe 'ChessGame' do
+  before :all do
+    @board = ChessGame.new
+  end
 
-  context 'only two white pieces on the board' do
-    before do
-      @two = ChessGame.new
-      @two.board = [[' '] * 8,
-                    [' '] * 8,
-                    [Piece.new("\u2659", 'white', 'pawn', [2, 0])] + [' '] * 7,
-                    [' '] * 8,
-                    [' '] * 8,
-                    [Piece.new("\u2659", 'white', 'pawn', [5, 0])] + [' '] * 7,
-                    [' '] * 8,
-                    [' '] * 8]
-    end
+  describe '#occupied_spaces'  do
 
-    it 'returns the right position of the pieces on the board' do
-      expect(@two.occupied_spaces('white').sort).to eql([[2, 0], [5, 0]])
+    context 'only two white pieces on the board' do
+      before do
+        @two = ChessGame.new
+        @two.board = [[' '] * 8,
+                      [' '] * 8,
+                      [Piece.new("\u2659", 'white', 'pawn', [2, 0])] + [' '] * 7,
+                      [' '] * 8,
+                      [' '] * 8,
+                      [Piece.new("\u2659", 'white', 'pawn', [5, 0])] + [' '] * 7,
+                      [' '] * 8,
+                      [' '] * 8]
+      end
+
+      it 'returns the right position of the pieces on the board' do
+        expect(@two.occupied_spaces('white').sort).to eql([[2, 0], [5, 0]])
+      end
     end
   end
 
+  describe '#move_legality' do
+
+    context 'when trying to move towers to a ilegal position do'
+
+      it 'returns false' do
+        expect(@board.move_legality([7, 7], [4, 7], 'black')).to eql(false)
+        expect(@board.move_legality([7, 0], [7, 2], 'black')).to eql(false)
+        expect(@board.move_legality([0, 0], [4, 0], 'white')).to eql(false)
+        expect(@board.move_legality([0, 7], [4, 7], 'white')).to eql(false)
+      end
+
+    end
+
+    context 'when trying to move horses to legal position' do
+
+      it 'returns true' do
+        expect(@board.move_legality([7, 6], [5, 7], 'black')).to eql(true)
+        expect(@board.move_legality([7, 1], [5, 2], 'black')).to eql(true)
+        expect(@board.move_legality([0, 1], [2, 0], 'white')).to eql(true)
+        expect(@board.move_legality([0, 6], [2, 5], 'white')).to eql(true)
+      end
+
+    end
 
 end
